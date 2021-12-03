15e376ab1d2c58b42ad9a0a67fd3348950c0783e
Glitch (rectangular-sulfuric-winterberry) <none>
2021-12-03 08:10:56 +0000

    Import of riyanest/propsearchbackend

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const env = require('dotenv');
+const cors = require('cors');
+const { json } = require('express');
+const app = express();
+const bodyParser =require('body-parser');
+app.use(cors());
+app.use(bodyParser.urlencoded({ 
+    extended: true 
+}));
+app.use(bodyParser.json());
+const mongoose = require('mongoose'); 
+const result = env.config();
+env.config();
+ 
+
+mongoose.connect(
+     'mongodb+srv://lappy:7MZlsX2l8DylW2GL@propsearchbackend.yohsd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+      }).then(()=>{
+          console.log("database connected"); 
+    });
+
+app.use('/auth',require('./routes/auth'));
+app.use('/data', require('./routes/data'));   
+
+app.get('/',(req,res,nest)=>{
+    res.status(200).json({
+        message: 'hello from server'
+    });
+});
+
+app.listen(process.env.PORT,()=>{
+    if (result.error) {
+        throw result.error
+      }
+    console.log(result.parsed);
+    console.log(`server is running on port ${process.env.PORT}`);
+}); 
