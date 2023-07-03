4e4196db68482eedadb573afc2002962ac8b94df
Glitch (rectangular-sulfuric-winterberry) <none>
2023-07-03 01:14:27 +0000

    🐏🚵 Checkpoint ./routes/data.js:39337782/4798

--- routes/data.js
+++ routes/data.js
@@ -22,6 +22,49 @@ var transporter = nodemailer.createTransport({
 });
 
 
+const csv = require('fast-csv'); // parses CSV files
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const main = async () => {
+
+    // connect to db
+    // const mongoDB = process.env.MONGODB_URI; 
+    // mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+    // const db = mongoose.connection;
+    // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+    // read csv file
+    const prop = [];
+    fs.createReadStream(path.join(__dirname, '/posts.csv'))
+        .pipe(csv.parse({ headers: true }))
+        .on('error', error => console.error(error))
+        .on('data', function(data) {
+            data['_id'] = new mongoose.Types.ObjectId();
+            data['author'] = new mongoose.Types.(data.author);
+            data['date'] = Date.now();
+            data['published'] = Boolean(data.published);
+            prop.push(data);
+        })
+        .on('end', function(){
+            // insert posts into db
+            property.insertMany(prop, function(err, documents) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            console.log(`${posts.length} + posts have been successfully uploaded.`);
+            return;
+        });
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exit();
+  });
+
 
 router.post("/mail", async function (req, res) {
   if (
