34bcdcaa66912167299efd413c7d48b6e0214d90
Glitch (rectangular-sulfuric-winterberry) <none>
2023-07-03 01:02:27 +0000

    🍕🚘 Checkpoint ./models/property.js:39337782/903

--- models/property.js
+++ models/property.js
@@ -2,10 +2,52 @@ const mongoose = require("mongoose");
 
 const property = new mongoose.Schema(
   {
-    apartmentType: {
+    bhk: {
       type: String,
       required: true
     },
+        address: {
+      type: String,
+      required: true
+    },
+            area: {
+      type: String,
+      required: true
+    },
+          price: {
+      type: String,
+    },
+            parking: {
+      type: String,
+      required: true    },
+            community: {
+      type: String,
+                    required: true
+    },
+            name: {
+      type: String,
+      required: true
+    },
+            number: {
+      type: String,
+      required: true
+    },
+            type: {
+      type: String,
+      required: true
+    },
+            description: {
+      type: String,
+      required: true
+    },
+
+
+
+
+
+
+
+
   },
   { timstamp: true }
 );
