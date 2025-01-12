a28c7a5b1747806b5a862dfc479ea6f0759851c7
Glitch (rectangular-sulfuric-winterberry) <none>
2025-01-12 10:12:37 +0530

    Revert "lets sse"
    This reverts commit f57a7f12f9d0fbd9c752cc03baaadebae73dffc1.

--- index.js
+++ index.js
@@ -32,5 +32,11 @@ app.get("/", (req, res, nest) => {
   });
 });
 
-
+app.listen(process.env.PORT, () => {
+  if (result.error) {
+    throw result.error;
+  }
+  console.log(result.parsed);
+  console.log(`server is running on port ${process.env.PORT}`);
+});
 module.exports = app;
